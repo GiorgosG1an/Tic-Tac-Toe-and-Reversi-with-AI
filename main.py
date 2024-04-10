d3823f9688f76d225f6201fe991276fb12981fb6
Georgios Giannopoulos <dit20039@go.uop.gr>
2024-04-10 13:56:23 +0300

    Update main.py to use alpha_beta_player instead of minmax_player

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from gamestate.gamestate import GameState
 from game.game import Game
 from game.tic_tac_toe import TicTacToe
-from players.players import manual_player, minmax_player, random_player
+from players.players import manual_player, minmax_player, random_player, alpha_beta_player
 
 def main():
     game = TicTacToe()
-    utility = game.play_game(minmax_player, random_player)
+    utility = game.play_game(alpha_beta_player, random_player)
     if utility == 1:
         print("'X' won!")
     elif utility == -1:
