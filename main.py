838e6322a67ddbc4c33be05840b53f577e494866
Georgios Giannopoulos <dit20039@go.uop.gr>
2024-04-10 17:39:24 +0300

    Update main.py to allow the user to choose between playing Tic Tac Toe or Reversi and add functionality to keep track of wins and ties

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ from gamestate.gamestate import GameState
 from game.game import Game
 from game.tic_tac_toe import TicTacToe
 from game.reversi import Reversi
-from players.players import manual_player, minmax_player, random_player, alpha_beta_player
+from players.players import manual_player, minmax_player, random_player, alpha_beta_player, reversi_random_player
 
 def main():
+    x_wins = 0
+    o_wins = 0
+    ties = 0
 
     print("Which game would you like to play?\n (1) Tic Tac Toe\n (2) Reversi")
     
@@ -19,20 +22,28 @@ def main():
                 print("'O' won!")
             else:
                 print('Tie!')
-            return
+            break
+    
         elif game_choice == '2':
             game = Reversi()
-
-            utility = game.play_game(manual_player, random_player)
-            if utility == 1:
-                print("'X' won!")
-            elif utility == -1:
-                print("'O' won!")
-            else:
-                print('Tie!')
-            return
+            for i in range(100):
+                utility = game.play_game(random_player, random_player)
+                if utility == 1:
+                    print("'X' won!")
+                    x_wins += 1
+                elif utility == -1:
+                    print("'O' won!")
+                    o_wins += 1
+                else:
+                    print('Tie!')
+                    ties += 1
+            break
         else:
             print("Invalid choice.\n")
+        
+    print("X wins: ", x_wins)
+    print("O wins: ", o_wins)
+    print("Ties: ", ties)
     
 if __name__ == "__main__":
     main()
