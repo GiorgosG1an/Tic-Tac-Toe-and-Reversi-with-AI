985153b1524a7b04f893340a4504f37870c1af17
Georgios Giannopoulos <dit20039@go.uop.gr>
2024-04-10 13:22:46 +0300

    Add main.py with game set to TicTacToe and players the minmax and the manual

---
+++ main.py
@@ -0,0 +1,18 @@
+from gamestate.gamestate import GameState
+from game.game import Game
+from game.tic_tac_toe import TicTacToe
+from players.players import manual_player, minmax_player
+
+def main():
+    game = TicTacToe()
+    utility = game.play_game(minmax_player, manual_player)
+    if utility == 1:
+        print("'X' won!")
+    elif utility == -1:
+        print("'O' won!")
+    else:
+        print('Tie!')
+
+
+if __name__ == "__main__":
+    main()
