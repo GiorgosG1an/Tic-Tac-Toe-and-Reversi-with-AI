37459dbd327e81f393c91772ee7db521d01b9126
Georgios Giannopoulos <dit20039@go.uop.gr>
2024-04-10 14:25:48 +0300

    Update main.py to allow the user to choose between playing Tic Tac Toe or Reversi

--- main.py
+++ main.py
@@ -1,18 +1,38 @@
 from gamestate.gamestate import GameState
 from game.game import Game
 from game.tic_tac_toe import TicTacToe
+from game.reversi import Reversi
 from players.players import manual_player, minmax_player, random_player, alpha_beta_player
 
 def main():
-    game = TicTacToe()
-    utility = game.play_game(alpha_beta_player, random_player)
-    if utility == 1:
-        print("'X' won!")
-    elif utility == -1:
-        print("'O' won!")
-    else:
-        print('Tie!')
 
+    print("Which game would you like to play?\n (1) Tic Tac Toe\n (2) Reversi")
+    
+    while(True):
+        game_choice = input("Enter the number of the game you would like to play: ")
+        if game_choice == '1':
+            game = TicTacToe()
+            utility = game.play_game(alpha_beta_player, random_player)
+            if utility == 1:
+                print("'X' won!")
+            elif utility == -1:
+                print("'O' won!")
+            else:
+                print('Tie!')
+            return
+        elif game_choice == '2':
+            game = Reversi()
 
+            utility = game.play_game(manual_player, random_player)
+            if utility == 1:
+                print("'X' won!")
+            elif utility == -1:
+                print("'O' won!")
+            else:
+                print('Tie!')
+            return
+        else:
+            print("Invalid choice.\n")
+    
 if __name__ == "__main__":
     main()
